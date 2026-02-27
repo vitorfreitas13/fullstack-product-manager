5de04d547ade64bc4cfa6c8d09babd7599317e81
Vitor Freitas <dev.vitorfreitas13@gmail.com>
2026-02-27 13:23:42 -0300

    criando tabela direto do node

--- back-end/server.js
+++ back-end/server.js
@@ -21,6 +21,20 @@ db.connect((err) => {
         console.log("Erro ao conectar:", err);
     } else {
         console.log("MySQL conectado ✅");
+        
+        // COMANDO PARA CRIAR A TABELA AUTOMATICAMENTE
+        const sql = `
+            CREATE TABLE IF NOT EXISTS produtos (
+                id INT AUTO_INCREMENT PRIMARY KEY,
+                nome VARCHAR(255) NOT NULL,
+                preco DECIMAL(10,2) NOT NULL,
+                quantidade INT NOT NULL
+            );`;
+            
+        db.query(sql, (err) => {
+            if (err) console.log("Erro ao criar tabela:", err);
+            else console.log("Tabela 'produtos' pronta para uso! 🚀");
+        });
     }
 });
 
